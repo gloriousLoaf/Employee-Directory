178ec75102e92d765f3b166cea1213012975583d
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-14 15:02:15 -0700

    Created Footer component, adjusted Header styling.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 // App
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/index.js';
+import Footer from './components/Footer/index.js'
 
 function App() {
   return (
     <div className="App">
       <div className="container"></div>
       <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+// Footer
+import React from 'react';
+import './style.css'
+
+function Footer() {
+    return (
+        <footer className='footerStyle'>
+            <div>
+                <i className='fab fa-github'></i> <a href='https://github.com/gloriousLoaf' target='_blank' rel='noopener noreferrer'>gloriousLoaf</a> &copy;2020
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
