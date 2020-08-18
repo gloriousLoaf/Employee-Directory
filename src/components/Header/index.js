0431b6fa1cea800b0bba8ba1a189beb413d22aa7
gloriousLoaf <davidmetcalfwork@gmail.com>
2020-08-18 09:44:21 -0700

    Updated header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ import './style.css'
 function Header() {
     return (
         <header className='headerStyle'>
-            <h1><span><img className='logoImg' src={Logo} alt='Contact book icon.'></img></span>Employee Directory</h1>
+            <h1><span><img className='logoImg' src={Logo} alt='Contact book icon.'></img></span>React Employee Directory</h1>
         </header>
     )
 };
